e9a743fefde1eca059515dddf1b6634b0e1e24ea
Carlos Andre <contato.carlosdede@gmail.com>
2025-05-17 02:05:58 -0300

    att img biblioteca

--- src/app/intranet/vagas/page.jsx
+++ src/app/intranet/vagas/page.jsx
@@ -159,7 +159,11 @@ export default function Page() {
           </nav>
         </header>
         <div className="banner">
-          <img src="/intranet/images/assinatura_akrus.png" alt="Banner" />
+          <img
+            id="imgbanner"
+            src="/intranet/images/assinatura_akrus.png"
+            alt="Banner"
+          />
         </div>
 
         <section>
