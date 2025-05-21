c804ddd894c343f13558e1036c0d5d3bb5b7ba6b
Carlos Andre <contato.carlosdede@gmail.com>
2025-05-20 21:20:06 -0300

    img

--- src/app/intranet/bibliotecas/page.jsx
+++ src/app/intranet/bibliotecas/page.jsx
@@ -172,7 +172,7 @@ export default function page() {
 
         <div className="banner">
           <img
-            src="/intranet/images/Bibliotecas_inter.jpg"
+            src="/intranet/images/bannerbiblioteca.jpg"
             alt="Banner"
             className="banner-image"
           />
